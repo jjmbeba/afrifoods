bd7d50cd473bff1084970a6ce8403a3580bede70
John Johnson <jjmbeba@gmail.com>
2025-11-03 17:27:41 +0300

    adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -12,6 +13,7 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
+    nitroV2Plugin(),
     viteReact(),
   ],
 });
